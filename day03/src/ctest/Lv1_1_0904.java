6046b0f2b0d3a8f26ea0afa181d95ec29c8f1650
DESKTOP-41M6VGO\user <comtaek@gmail.com>
2024-09-05 09:08:01 +0900

    0905

---
+++ day03/src/ctest/Lv1_1_0904.java
@@ -0,0 +1,19 @@
+package ctest;
+
+import java.util.Arrays;
+
+public class Lv1_1_0904 {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Lv1_1_0904 solution = new Lv1_1_0904();
+        int[] answer = solution.solution(new int[]{1, 1,3,3,0,1,2});
+        System.out.println(Arrays.toString(answer));
+    }
+}
