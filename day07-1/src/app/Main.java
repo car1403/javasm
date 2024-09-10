f453cd7dc5b22373e1fb1b1cc9a3de487e257490
DESKTOP-41M6VGO\user <comtaek@gmail.com>
2024-09-10 13:46:36 +0900

    0910

--- day07-1/src/app/Main.java
+++ day07-1/src/app/Main.java
@@ -1,13 +1,36 @@
 package app;
 
 import dto.Cust;
+import dto.Item;
 import frame.Db;
 import repository.CustDb;
+import repository.ItemDb;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Db db = new CustDb();
+        Db<String, Cust> db = new CustDb();
         Cust cust = new Cust("id01","pwd01","james");
         db.insert(cust);
+
+        String id = "id11";
+        Cust dbCust = db.select(id);
+        System.out.println(dbCust);
+
+        List<Cust> custs = db.select();
+        for(Cust c:custs){
+            System.out.println(c);
+        }
+
+
+
+
+        Db db2 = new ItemDb();
+        Item item = new Item(100,"pants",20000);
+        db2.insert(item);
     }
 }
+
+
+
--- day07-1/src/frame/Db.java
+++ day07-1/src/frame/Db.java
@@ -1,7 +1,11 @@
 package frame;
 
+import java.util.List;
+
 public interface Db<K,V> {
     void insert(V v);
     void update(V v);
     void delete(K k);
+    V select(K k);
+    List<V> select();
 }
